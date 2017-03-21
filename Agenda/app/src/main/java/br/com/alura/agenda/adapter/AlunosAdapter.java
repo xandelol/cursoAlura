1a94366b167946db97d1cf7735a0244757f0f159
Alexandre A Cardoso <Alexandre A Cardoso>
2017-03-21 15:34:06 -0300

    Layout atualizado

--- Agenda/app/src/main/java/br/com/alura/agenda/adapter/AlunosAdapter.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/adapter/AlunosAdapter.java
@@ -59,6 +59,14 @@ public class AlunosAdapter extends BaseAdapter {
         TextView campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
         campoTelefone.setText(aluno.getTelefone());
 
+        TextView campoEndereco = (TextView)view.findViewById(R.id.item_endereco);
+        if (campoEndereco != null)
+            campoEndereco.setText(aluno.getEndereco());
+
+        TextView campoSite = (TextView)view.findViewById(R.id.item_site);
+        if (campoSite != null)
+            campoSite.setText(aluno.getSite());
+
         ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
         String caminhoFoto = aluno.getCaminhoFoto();
         if (caminhoFoto != null){
